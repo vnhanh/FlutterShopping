d594234f19c870477e9480c7bb12e145c1c22552
vnhanh <nghiamy15@gmail.com>
2023-07-10 03:23:56 +0700

    feat(login): add form

--- lib/presentations/auth/login/login.dart
+++ lib/presentations/auth/login/login.dart
@@ -24,6 +24,17 @@ class LoginScreenStatefulWidget extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreenStatefulWidget> {
+  final _formKey = GlobalKey<FormState>();
+  String? _email = null;
+  String _password = "";
+
+  void _submit() {
+      if (_formKey.currentState?.validate() == true) {
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("Submit SignIn")),
+          );
+      }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,29 +76,84 @@ class _LoginScreenState extends State<LoginScreenStatefulWidget> {
   }
 
   Widget inputFields() {
-    return Column(
-        children: [
+    return Form(
+        key: _formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Column(
+            children: [
+            // Input Email
             Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: TextField(
+                child: TextFormField(
                     decoration: InputDecoration(
-                        prefixIcon: const Icon(Icons.account_box),
-                        border: const OutlineInputBorder(),
-                        hintText: AppLocalizations.of(context)?.loginEmailHint ?? ""
+                      prefixIcon: const Icon(Icons.account_box),
+                      border: const OutlineInputBorder(),
+                      label: Padding(
+                        padding: const EdgeInsets.only(top: 4.0),
+                        child: Text(AppLocalizations.of(context)?.loginEmailHint ?? ""),
+                      ),
                     ),
+                    validator: (value) {
+                        if (value == null || value.length < 6) {
+                            return AppLocalizations.of(context)?.loginEmailInvalid ?? "";
+                        }
+                        return null;
+                    },
+                    onChanged: (text) {
+                        setState(() => _email = text);
+                    },
                 ),
             ),
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: TextField(
-                  decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.lock),
-                      border: const OutlineInputBorder(),
-                      hintText: AppLocalizations.of(context)?.loginPasswordHint ?? ""
+            // Input Password
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.lock, semanticLabel: "Semantic label",),
+                    focusColor: Colors.blue,
+                    border: const OutlineInputBorder(),
+                    label: Padding(
+                      padding: const EdgeInsets.only(top: 4.0),
+                      child: Text(AppLocalizations.of(context)?.loginPasswordHint ?? ""),
+                    ),
                   ),
-              ),
-          )
-        ],
+                ),
+            ),
+            Padding(
+                padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 24.0, bottom: 116.0),
+                child: ElevatedButton(
+                    onPressed: _submit,
+                    style: ButtonStyle(
+                        overlayColor: MaterialStateProperty.resolveWith((states) {
+                            if (states.contains(MaterialState.pressed)) {
+                                return Colors.lightBlue.withOpacity(0.5);
+                            }
+                            return null;
+                        }),
+                        foregroundColor: MaterialStateProperty.resolveWith((states) {
+                            if (states.contains(MaterialState.values)) {
+                                return Colors.lightBlue;
+                            }
+                            return null;
+                        }),
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                        )),
+                        shadowColor: MaterialStateProperty.all(Colors.black.withOpacity(0.4)),
+                        elevation: MaterialStateProperty.all(8.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                      child: Text(
+                        AppLocalizations.of(context)?.loginButton ?? "SIGN IN",
+                        style: mobileTitleMediumSemiBold.copyWith(color: Colors.white),
+                      ),
+                    ),
+                ),
+            )
+          ],
+        ),
     );
   }
 }
