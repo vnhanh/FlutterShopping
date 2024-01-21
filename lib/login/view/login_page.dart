ac6b3e72a8b76bd0b905ff08a40f4449965019b0
Alan Vo <nghiamy15@gmail.com>
2024-01-21 21:32:01 +0700

    feat(login): show image at screen header

--- lib/login/view/login_page.dart
+++ lib/login/view/login_page.dart
@@ -23,18 +23,15 @@ class LoginView extends StatelessWidget {
   Widget header() {
     return Container(
       width: double.infinity,
-      height: 120.0,
+      height: 350.0,
       decoration: const BoxDecoration(
         gradient: LoginGradient.loginBgGradient,
       ),
       child: const Align(
         alignment: Alignment.center,
         child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 8.0),
-          child: Text(
-            "Shoppo",
-            style: TextStyle(color: Colors.red, fontSize: 20.0, fontWeight: FontWeight.bold),
-          ),
+          padding: EdgeInsets.symmetric(vertical: 16.0),
+          child: Image(image: AssetImage('assets/images/login/shopping-header-login-image.png'))
         ),
       ),
     );
@@ -42,7 +39,7 @@ class LoginView extends StatelessWidget {
 
   Widget bodyCard() {
     return Container(
-      margin: const EdgeInsets.only(top: 100.0),
+      margin: const EdgeInsets.only(top: 330.0),
       width: double.infinity,
       // margin: const EdgeInsets.only(top: 32.0),
       decoration: const BoxDecoration(
@@ -53,29 +50,12 @@ class LoginView extends StatelessWidget {
         mainAxisSize: MainAxisSize.max,
         children: [
           Positioned(
-              top: 20.0,
+              top: 32.0,
               child: LoginForm()
           )
         ],
       ),
     );
   }
-
-  // Widget bodyCard() {
-  //   return Positioned(
-  //     top: 100.0,
-  //     child: Text("Text"),
-  //     child: Container(
-  //       width: double.infinity,
-  //       height: double.infinity,
-  //       // margin: const EdgeInsets.only(top: 32.0),
-  //       decoration: const BoxDecoration(
-  //         borderRadius: BorderRadius.only(topLeft: Radius.circular(32.0), topRight: Radius.circular(32.0)),
-  //         color: Colors.white,
-  //       ),
-  //       child: const LoginForm(),
-  //     ),
-  //   );
-  // }
 }
 
