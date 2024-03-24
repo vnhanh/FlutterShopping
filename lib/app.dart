943ab86219d377bc3e370a12bd773b0d2b8a1e0c
Alan Vo <nghiamy15@gmail.com>
2024-03-24 18:26:55 +0700

    build: upgrade dependencies

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:shopping/authentication/domain/authentication_bloc.dart';
 import 'package:shopping/authentication/data/repository/authentication_repository.dart';
@@ -17,6 +18,7 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  SharedPreferences? sp;
   late final AuthenticationRepository _authenticationRepository;
   late final UserRepository _userRepository;
 
